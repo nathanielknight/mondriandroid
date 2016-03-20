2fd97195eca092d3261c96b14f3789ffa0066a83
Nathaniel Knight <nknight@siaras.com>
2016-03-20 00:03:49 -0700

    fix content type

--- src/mondriandroid/handler.clj
+++ src/mondriandroid/handler.clj
@@ -1,10 +1,8 @@
 (ns mondriandroid.handler
   (:require [mondriandroid.generate :as g]
-            [mondriandroid.draw :as d])
-  )
-
+            [mondriandroid.draw :as d]))
 
 (defn dev [req]
   {:status 200
-   :headers {"Cnotent-Type" "image/svg+xml"}
+   :headers {"Content-Type" "image/svg+xml"}
    :body (d/render (g/generate))})
