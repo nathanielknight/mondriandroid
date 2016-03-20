3354dd6d8f416923203e424c2ec05a4f29b3d71f
Nathaniel Knight <nknight@siaras.com>
2016-03-20 00:04:08 -0700

    playing with generation algos

--- src/mondriandroid/generate.clj
+++ src/mondriandroid/generate.clj
@@ -5,6 +5,14 @@
   (:import [org.apache.commons.math3.distribution NormalDistribution]))
 
 
+
+(defn random-weights [weights]
+  (rand-nth
+   (apply
+    concat
+    (for [[val count] weights]
+            (repeat count val)))))
+
 (defn split-point [] 0.5)
 (defn split-std [] 0.1)
 
@@ -18,31 +26,57 @@
 (defn- normal [mean std]
   (.sample (NormalDistribution. mean std)))
 
-(defn- split-across [r]
+(defn- split-across
+  "Split a rect into four rects in a cross shape"
+  [r]
   (let [rx (normal (split-point) (split-std))
         ry (normal (split-point) (split-std))]
     (mapcat
      #(split-y % ry)
      (split-x r rx))))
 
-(defn- split-vert [r]
+(defn- split-vert
+  "Split a rect vertically"
+  [r]
   (let [ry (normal (split-point) (split-std))]
     (split-y r ry)))
 
-(defn- split-horiz [r]
+(defn- split-horiz
+  "Split a rect horizontally"
+  [r]
   (let [rx (normal (split-point) (split-std))]
     (split-x r rx)))
+
+(defn- needs-remediation? [r]
+  (> (-> r
+         aspect-ratio
+         java.lang.Math/log
+         java.lang.Math/abs)
+     0.5))
+
+(defn- remedial-split
+  "Split a rect in such a way that its aspect ratio comes back towards 1"
+  [rect]
+  (if (< 0 (java.lang.Math/log (aspect-ratio rect)))
+    (split-horiz rect)
+    (split-vert rect)))
 
 (defn- split
   "Given a rect, split it horizontally or vertically into a few rects
   which tile the original."
   [r]
-  (randomly
-   (split-across r)
-   (split-vert r)
-   (split-horiz r)))
+  (if (needs-remediation? r)
+    (remedial-split r)
+    ((random-weights
+      {
+;       split-across 1
+       split-vert 3
+       split-horiz 3
+       }
+      ) r)))
 
-(defn- terminate "Given a rect, return it with an optional colour."
+(defn- terminate
+  "Given a rect, return it with an optional colour."
   [r]
   (list {:rect r
          :colour (colour/random-colour)}))
@@ -50,14 +84,17 @@
 (defn generate
   "Given a rect, generate a Mondrianesque which tiles it."
   ([]
-   (generate (rect (point 0 0) (point 10 6.2))))
+   (generate (rect (point 0 0) (point 20 12.4))))
   ([r]
    (generate r 0))
   ([r n]
    (cond
-     (<= n 0) (mapcat #(generate % (+ n 1)) (split-across r))
-     (<= n 1) (mapcat #(generate % (+ n 1)) (split-horiz r))
-     (<= n 2) (randomly
-               (terminate r)
-               (mapcat #(generate % (+ n 1) ) (split r)))
-     :else (terminate r))))
+     (<= n 2) (mapcat #(generate % (+ n 1)) (split r))     
+     (<= n 8)    ((random-weights
+                   {terminate n
+                    (fn [r] (mapcat #(generate % (+ n 1)) (split r))) 12})
+                  r)
+     :else (terminate r))
+   ))
+
+
