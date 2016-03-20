305aa71e3ae42108668d7a6c449577608c70fd49
Nathaniel Knight <nknight@siaras.com>
2016-03-20 00:01:20 -0700

    bump commons math to latest version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.apache.commons/commons-math3 "3.2"]]
+                 [org.apache.commons/commons-math3 "3.6"]]
   :profiles {:dev {:dependencies [[ring/ring-core "1.4.0"]
                                   [ring/ring-jetty-adapter "1.4.0"]]
                    :plugins [[cider/cider-nrepl "0.8.2"]
