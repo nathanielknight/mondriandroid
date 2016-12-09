49c6eb7056f4049446e15c0bed90169d28b4c13a
Nathaniel Knight <nknight@siaras.com>
2016-12-09 15:59:26 -0800

    Add `bidi` routing
    This commit adds a bidi route (cum ring handler) to the server.

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.immutant/web "2.1.5"]
-                 [ring/ring-core "1.5.0"]
                  [org.apache.commons/commons-math3 "3.6"]
                  [bidi "2.0.14"]]
   :profiles {:dev {:dependencies [[ring/ring-devel "1.5.0"]
--- src/mondriandroid/core.clj
+++ src/mondriandroid/core.clj
@@ -1,12 +1,23 @@
 (ns mondriandroid.core
   (:require [mondriandroid.draw :as draw]
             [mondriandroid.generate :as generate]
-            [immutant.web :as iweb]))
+            [immutant.web :as iweb]
+            [bidi.ring :as br]))
 
-(defn handler [req]
+(defn app-handler [req]
   {:status 200
    :headers {"Content-Type" "image/svg+xml"}
    :body (draw/render (generate/generate))})
+
+(defn missing-handler [req]
+  {:status 404
+   :headers {"Content-Type" "text/plain"}
+   :body (str "I can't find " (:uri req))})
+
+(def app-routes ["/" {"app" app-handler
+                      true missing-handler}])
+
+(def app (br/make-handler app-routes))
 
 (defn -main
   ([]
